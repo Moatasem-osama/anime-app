76a7d1f6ea9252710a067ba6baed315db5598cac
Moatasem Osama <asomyosamalahawary@gmail.com>
2026-01-30 22:47:14 +0200

    v2

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,32 +1,39 @@
-// "use client";
-// import Link from "next/link";
-// import { useEffect, useState } from "react";
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Products() {
-    // const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-    // async function getAnime() {
-    //     const res = await fetch("https://api.jikan.moe/v4/anime");
-    //     const result = await res.json();
-    //     setData(result.data);
-    // }
+    async function getAnime() {
+        const res = await fetch("https://api.jikan.moe/v4/anime?status=airing");
+        const result = await res.json();
+        setData(result.data);
+    }
 
-    // useEffect(() => {
-    //     getAnime();
-    // }, []);
+    useEffect(() => {
+        getAnime();
+    }, []);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {/* {data.map(item => (
+        <section className="">
+        <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 container mx-auto">
+            {data.map(item => (
                 <Link
             key={item.mal_id}
             href={item.url}
-            className="p-4 border rounded cursor-pointer hover:shadow-lg transition"
+            className="p-4 border border-white text-white rounded cursor-pointer hover:shadow-lg transition"
             >
-            <img src={item. images.webp.image_url} alt="" className="w-full object-cover h-80" />
+             <div
+  className="w-64 h-80 bg-cover bg-center rounded-lg shadow-lg"
+  style={{ backgroundImage: `url(${item.images.webp.image_url})` }}
+>
             <h3 className="text-center">{item.title.split(" ",2).join(" ")}</h3>
+</div>
+
         </Link>
-            ))} */}
+            ))}
         </div>
+        </section>
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 w-full bg-white/95 backdrop-blur-sm shadow-sm">
+    <header className="fixed top-0 left-0 w-full text-white shadow-sm">
       <div className="container mx-auto p-4">
         <div className="flex justify-between items-center ">
           <Link href={"/"} className="">
@@ -13,7 +13,7 @@ export default function Header() {
           </Link>
           <button className="md:hidden" onClick={() => setIsOpen((prev)=> !prev)}>
             {isOpen ? (
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={30} height={30} viewBox="0 0 24 24" style={{fill: '#1A1A1A'}}>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={30} height={30} viewBox="0 0 24 24" style={{fill: '#fff'}}>
   <path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z" />
 </svg>
 
@@ -26,7 +26,7 @@ export default function Header() {
               width={30}
               height={30}
               viewBox="0 0 50 50"
-              style={{ fill: "#1A1A1A" }}
+              style={{ fill: "#fff" }}
             >
               <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z" />
             </svg>
@@ -35,37 +35,37 @@ export default function Header() {
             }
           </button>
           {isOpen && (
-            <ul className="flex flex-col space-y-4 md:hidden absolute top-18 items-center justify-center left-0 w-full bg-white p-4 shadow-md ">
+            <ul className="flex flex-col space-y-4 md:hidden absolute top-18 items-center justify-center left-0 w-full  p-4 shadow-md bg-black/60">
               <li>
-                <Link className="text-gray-700 Capitalize " href={"/"}>
+                <Link className="text-white Capitalize " href={"/"}>
                   Home
                 </Link>
               </li>
               <li>
-                <Link className="text-gray-700 Capitalize " href={"/about"}>
+                <Link className="text-white Capitalize " href={"/about"}>
                   About
                 </Link>
               </li>
               <li>
-                <Link className="text-gray-700 Capitalize " href={"/products"}>
+                <Link className="text-white Capitalize " href={"/products"}>
                   Products
                 </Link>
               </li>
             </ul>
           )}
-          <ul className="hidden md:flex space-x-4">
+          <ul className="hidden md:flex space-x-4 text-white">
             <li>
-              <Link className="text-gray-700 Capitalize " href={"/"}>
+              <Link className="text-white Capitalize " href={"/"}>
                 Home
               </Link>
             </li>
             <li>
-              <Link className="text-gray-700 Capitalize " href={"/about"}>
+              <Link className="text-white Capitalize " href={"/about"}>
                 About
               </Link>
             </li>
             <li>
-              <Link className="text-gray-700 Capitalize " href={"/products"}>
+              <Link className="text-white Capitalize " href={"/products"}>
                 Products
               </Link>
             </li>
