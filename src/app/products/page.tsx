e6f91efc0beb934f0f05eca5944b8bbc1358ba9d
Moatasem Osama <asomyosamalahawary@gmail.com>
2026-01-30 21:53:26 +0200

    first commit

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,23 +1,23 @@
-"use client";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+// "use client";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
 
 export default function Products() {
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
 
-    async function getAnime() {
-        const res = await fetch("https://api.jikan.moe/v4/anime");
-        const result = await res.json();
-        setData(result.data);
-    }
+    // async function getAnime() {
+    //     const res = await fetch("https://api.jikan.moe/v4/anime");
+    //     const result = await res.json();
+    //     setData(result.data);
+    // }
 
-    useEffect(() => {
-        getAnime();
-    }, []);
+    // useEffect(() => {
+    //     getAnime();
+    // }, []);
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data.map(item => (
+            {/* {data.map(item => (
                 <Link
             key={item.mal_id}
             href={item.url}
@@ -26,7 +26,7 @@ export default function Products() {
             <img src={item. images.webp.image_url} alt="" className="w-full object-cover h-80" />
             <h3 className="text-center">{item.title.split(" ",2).join(" ")}</h3>
         </Link>
-            ))}
+            ))} */}
         </div>
     );
 }
